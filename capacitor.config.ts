d0667eb4d023ac04c75410e632fa734797c5713b
freelancingorganisationflo-del <freelancingorganisationflo@gmail.com>
2026-07-28 20:51:21 +0000

    Add Capacitor Android setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.flo.app',
+  appName: 'FLO',
+  webDir: 'dist'
+};
+
+export default config;
